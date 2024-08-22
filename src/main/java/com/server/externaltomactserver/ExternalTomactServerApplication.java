501bc068ac584f358b11767fec2550a463af3930
Vedant Agrawal <vedantagrawal647@gmail.com>
2024-08-22 16:12:22 +0530

    first

---
+++ src/main/java/com/server/externaltomactserver/ExternalTomactServerApplication.java
@@ -0,0 +1,13 @@
+package com.server.externaltomactserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalTomactServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalTomactServerApplication.class, args);
+    }
+
+}
